d53250e1ba759a093a0c1b995c84865254ecb5c3
akmsy <akmsyyy1@gmail.com>
2026-03-28 19:32:47 +0700

    update pemanggilan navbar

--- index.php
+++ index.php
@@ -9,21 +9,7 @@
 </head>
 <body>
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.php">Komunitas Kucing</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-link" href="index.php">Home</a>
-                    <a class="nav-link" href="login.php">Daftar</a>
-                    <a class="nav-link" href="design.php">Design</a>
-                </div>
-            </div>
-        </div>
-    </nav>
+    <?php include 'navbar.php' ?>
 
     <!-- Home Page -->
     <div class="hero-section m-5">
