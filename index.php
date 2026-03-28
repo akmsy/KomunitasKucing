7d1c439b34406ce7b60e9d87a58c3a7212a6e7c5
akmsy <akmsyyy1@gmail.com>
2026-03-28 09:19:26 +0700

    add halaman home

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <title>Komunitas Kucing</title>
+</head>
+<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">Komunitas Kucing</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link" href="index.php">Home</a>
+                    <a class="nav-link" href="daftar.php">Daftar</a>
+                    <a class="nav-link" href="design.php">Design</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
+    <!-- Home Page -->
+    <div class="hero-section m-5">
+        <h1 class="fst-italic mb-3">Selamat Datang di Komunitas Kucing</h1>
+        <div class="hero-content mb-3">
+            <img class="border border-5 rounded-5 border-warning" src="kucing.jpg" alt="foto-kucing">
+            <p class="text-danger mt-3">Kucing adalah hewan mamalia kecil yang dikenal dengan sifatnya yang licah, anggun, dan penuh rasa ingin tahu. Tubuhnya lentur dengan bulu lembut yang beragam warna, serta mata tajam yang mampu melihat jelas di kondisi minim cahaya. Kucing termasuk hewan karnivora, namun dapat beradaptasi hidup bersama manusia sebagai hewan peliharaan. Mereka memiliki kebiasaan mencakar untuk menandai wilayah, merawat diri dengan menjilat bulunya, serta tidur dalam waktu lama, bisa mencaai 12-16 jam per hari. Kucing sering dianggap membawa kenyamanan dan kasih sayang, sehingga menjadi salah satu hewan peliharaan paling populer di dunia.</p>
+
+            <p class="text-primary">Kucing seperti anjing termasuk hewan penyayang. Riseg yang dilakukan scientificamerican mengungkapkan kucing mempelajari sendiri cara mengeluarkan bunyi meow yang bisa menarik perhatian manusia. Berikut Macam-Macam Kucing:</p>
+
+            <h3>Jenis-Jenis Kucing:</h3>
+            <ol>
+                <li>Kucing Garong</li>
+                <li>Kucing Mainan</li>
+                <li>Kucing Tuan Muda</li>
+                <li>Kucing Nakal</li>
+                <li>dll.</li>
+            </ol>
+        </div>
+
+
+        <a class="btn btn-primary" href="wolipop.detik.com" role="button">Informasi Lebih Lanjut</a>
+</body>
+</html>
